2ab9b3b3d8151c559de3b8a64979ae273a284898
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2025-03-31 23:03:39 +0900

    coded peak_hours model with CTEs

---
+++ models/peak_hours.sql
@@ -0,0 +1,14 @@
+-- models/peak_hours.sql
+
+WITH hourly_trips AS (
+  SELECT
+    EXTRACT(HOUR FROM pickup_datetime) as pickup_hour,
+    COUNT(*) AS total_trips,
+
+  FROM {{ ref('clean_taxi_trips')}}
+  GROUP BY pickup_hour
+    )
+
+SELECT *
+FROM hourly_trips
+ORDER BY pickup_hour
