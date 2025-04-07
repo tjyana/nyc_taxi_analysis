7f35aac919b295524071500df02823c7444cb867
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2025-04-08 01:33:32 +0900

    practice window function ROW NUMBER

---
+++ models/window_fare_rank.sql
@@ -0,0 +1,12 @@
+SELECT
+  DATE(pickup_datetime) as ride_date,
+  fare_amount,
+  ROW_NUMBER() OVER (
+    PARTITION BY DATE(pickup_datetime)
+    ORDER BY fare_amount DESC
+  ) AS fare_rank
+
+  FROM {{ ref('clean_taxi_trips') }}
+
+-- This query selects the date of the ride and the fare amount from the clean_taxi_trips table.
+-- It then assigns a rank to each fare amount for each date, ordering them in descending order.
