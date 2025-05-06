b6a92dc8e2da3a6d7695ed74aa53fb3745ab4d6b
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2025-05-06 15:44:48 +0900

    corrected sql and added to mart

---
+++ models/mrt/mrt_window_tip_per_hour.sql
@@ -0,0 +1,30 @@
+-- Practice Problem 3: Calculating Average Tip per Hour and Ranking the Hours
+-- Scenario:
+-- Analyze the taxi trips by calculating the average tip amount for each hour of the day, then rank the hours by this average to identify peak tipping periods.
+
+-- SELECT
+--   EXTRACT(HOUR from pickup_datetime) as pickup_hour,
+--   tip_amount,
+--   AVG(tip_amount) OVER (
+--     PARTITION BY pickup_hour
+--     ORDER BY pickup_hour
+
+--   ) as tip_hourly_total
+
+
+WITH hourly_avg AS (
+  SELECT
+    EXTRACT(HOUR FROM pickup_datetime) AS pickup_hour,
+    AVG(tip_amount) AS avg_tip
+  FROM {{ ref('clean_taxi_trips') }}
+  GROUP BY EXTRACT(HOUR FROM pickup_datetime)
+)
+
+SELECT
+  pickup_hour,
+  avg_tip,
+  RANK() OVER (
+    ORDER BY avg_tip DESC
+  ) AS tip_rank
+FROM hourly_avg
+ORDER BY tip_rank;
